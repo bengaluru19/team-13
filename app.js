4d5fb9add081eb43876280b94a88adefdab5948a
hhk998402 <hemant.hari.kumar@gmail.com>
2019-07-06 17:58:24 +0530

    Create Event Route

--- app.js
+++ app.js
@@ -7,8 +7,6 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-require('dotenv').config({path: __dirname + '/.env'});
-
 var app = express();
 
 // view engine setup
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var admin = require("firebase-admin");
 
 var serviceAccount = require("../codeforgood-team13.json");
-require('dotenv').config({path: __dirname + '/.env'});
+require('dotenv').config({path: __dirname + '/../.env'});
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
@@ -12,9 +12,6 @@ admin.initializeApp({
 
 var db = admin.database();
 var ref = db.ref("code_for_good/events_schema");
-ref.once("value", function(snapshot) {
-    console.log(snapshot.val());
-});
 var eventsRef = ref.child("events");
 
 /* GET home page. */
@@ -23,15 +20,35 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/createEvent', function(req, res, next){
-  var date = req.body.date;
-  var name_of_event = req.body.name_of_event;
-  var address = req.body.address;
-  var volunteer_hours = req.body.volunteer_hours;
-  var skill_set = req.body.skill_set;
-  var no_of_volunteers_needed = req.body.no_of_volunteers_needed;
-  eventsRef.push({
+  console.log(req.body);
+  console.log("HERE");
 
+  ref.once("value", function(snapshot) {
+      console.log(snapshot.val());
   });
 
+  let date = req.body.date;
+  let name_of_event = req.body.name_of_event;
+  let address = req.body.address;
+  let volunteer_hours = req.body.volunteer_hours;
+  let skill_set = req.body.skill_set.split(',');
+  let no_of_volunteers_needed = req.body.no_of_volunteers_needed;
+  let lat = req.body.lat;
+  let long = req.body.long;
+  let latLong = {
+      lat:lat,
+      long:long
+  };
+  eventsRef.push({
+      date : date,
+      name_of_event : name_of_event,
+      address : address,
+      volunteer_hours : volunteer_hours,
+      skill_set : skill_set,
+      no_of_volunteers_needed : no_of_volunteers_needed,
+      location : latLong
+  });
+  res.send("Hello");
 });
+
 module.exports = router;
